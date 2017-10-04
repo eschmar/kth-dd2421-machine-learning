114b2eeeb53637c415665ad53b2c08a18090a489
Marcel Eschmann <marcel.e@nectima.se>
2017-10-04 16:16:02 +0200

    Merge frames and gif file

--- support-vector-machines/frames.py
+++
@@ -1,9 +0,0 @@
-import sys, helper, main, subprocess
-
-# Run
-if __name__ == '__main__':
-    params = helper.parseArguments(sys.argv)
-
-    for i in (x / 10 for x in range(1, 100)):
-        current = '%03d' % int(i * 10)
-        main.run(['--radial', '-sigma', str(i), '--save', '-o', "frame{0}.png".format(current)])
--- support-vector-machines/gif.py
+++ support-vector-machines/gif.py
@@ -1,11 +1,23 @@
 from os import listdir
 from os.path import isfile, join
 import imageio
+import sys, helper, main
 
-frames = [f for f in listdir("out/") if isfile(join("out/", f)) and f.startswith("frame")]
+# Run
+if __name__ == '__main__':
+    params = helper.parseArguments(sys.argv)
 
-images = []
-for frame in frames:
-    images.append(imageio.imread(join("out/", frame)))
+    if '--gif' not in params:
+        frames = []
+        for i in (x / 10 for x in range(1, 100)):
+            current = "frame{0}.png".format('%03d' % int(i * 10))
+            main.run(['--radial', '-sigma', str(i), '--save', '-o', current])
+            frames.append(current)
+    else:
+        frames = [f for f in listdir("out/") if isfile(join("out/", f)) and f.startswith("frame")]
 
-imageio.mimsave('out/movie.gif', images)
+    images = []
+    for frame in frames:
+        images.append(imageio.imread(join("out/", frame)))
+
+    imageio.mimsave('out/movie.gif', images)
